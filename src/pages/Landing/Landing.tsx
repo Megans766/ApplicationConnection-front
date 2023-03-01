8896cd4e06e4ae264562d7802254b7462fc0f75a
Megan Smith <megans766@gmail.com>
2023-03-01 10:15:20 -0500

    update all pages with favicon & update landing page

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -16,7 +16,7 @@ const Landing = (props: LandingProps): JSX.Element => {
       <div>
         <h1>Application Connection</h1>
         <p>Application Connection was designed with the job seeker in mind.</p>
-        <p>If your looking for a way to keep track of the applications you have submitted, search no more.</p>
+        <p>If you're looking for a way to keep track of the applications you have submitted, search no more.</p>
         <p>Application Connection is here to help.</p>
         <p><strong>Sign Up to get started.</strong></p>
       </div>
